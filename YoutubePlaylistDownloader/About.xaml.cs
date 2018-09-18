675904aa4e3c4af447379986d573982b40d3e5f4
shaked6540 <shaked6540@users.noreply.github.com>
2018-09-18 17:00:31 +0300

    Added check for updates button in 'About' page

--- YoutubePlaylistDownloader/About.xaml.cs
+++ YoutubePlaylistDownloader/About.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Net;
+using System.Windows.Controls;
 
 namespace YoutubePlaylistDownloader
 {
@@ -14,6 +17,31 @@ namespace YoutubePlaylistDownloader
             GlobalConsts.ShowHomeButton();
             GlobalConsts.ShowSettingsButton();
             AboutRun.Text += GlobalConsts.VERSION;
+        }
+
+        private async void UpdateButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                using (var wc = new WebClient())
+                {
+                    var latestVersion = double.Parse(await wc.DownloadStringTaskAsync("https://raw.githubusercontent.com/shaked6540/YoutubePlaylistDownloader/master/YoutubePlaylistDownloader/latestVersion.txt"));
+
+                    if (latestVersion > GlobalConsts.VERSION)
+                    {
+                        var changelog = await wc.DownloadStringTaskAsync("https://raw.githubusercontent.com/shaked6540/YoutubePlaylistDownloader/master/YoutubePlaylistDownloader/changelog.txt");
+                        var update = await GlobalConsts.ShowYesNoDialog($"{FindResource("NewVersionAvailable")}", $"{FindResource("DoYouWantToUpdate")}\n{changelog}");
+                        if (update == MessageDialogResult.Affirmative)
+                            GlobalConsts.LoadPage(new DownloadUpdate(latestVersion));
+                    }
+                    else
+                        await GlobalConsts.ShowMessage($"{FindResource("NoUpdates")}", $"{FindResource("NoUpdatesAvailable")}");
+                }
+            }
+            catch (Exception ex)
+            {
+                await GlobalConsts.ShowMessage($"{FindResource("Error")}", $"{FindResource("CannotUpdate")} {ex.Message}");
+            }
         }
     }
 }
