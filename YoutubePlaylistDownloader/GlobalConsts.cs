bb7eebd282b367af4187ac2287ea4021ca8ab64a
shaked6540 <shaked6540@users.noreply.github.com>
2019-04-22 14:10:36 +0300

    Fixed a critical bug, added Deutsch translation

--- YoutubePlaylistDownloader/GlobalConsts.cs
+++ YoutubePlaylistDownloader/GlobalConsts.cs
@@ -33,7 +33,7 @@ namespace YoutubePlaylistDownloader
         public static readonly string CurrentDir;
         private static readonly string ConfigFilePath;
         private static readonly string ErrorFilePath;
-        public const double VERSION = 1.707;
+        public const double VERSION = 1.708;
         public static bool UpdateOnExit;
         public static string UpdateSetupLocation;
         public static bool OptionExpanderIsExpanded;
