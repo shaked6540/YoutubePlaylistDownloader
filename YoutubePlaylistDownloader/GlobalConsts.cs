41f894ec85d4419ed071664670adaabc3c31adc7
shaked6540 <shaked6540@users.noreply.github.com>
2020-03-13 14:42:50 +0200

    Updated libraries and version

--- YoutubePlaylistDownloader/GlobalConsts.cs
+++ YoutubePlaylistDownloader/GlobalConsts.cs
@@ -33,7 +33,7 @@ namespace YoutubePlaylistDownloader
         public static readonly string CurrentDir;
         private static readonly string ConfigFilePath;
         private static readonly string ErrorFilePath;
-        public const double VERSION = 1.809;
+        public const double VERSION = 1.810;
         public static bool UpdateOnExit;
         public static string UpdateSetupLocation;
         public static bool OptionExpanderIsExpanded;
