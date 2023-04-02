45a5cb6a65c8895dfa99706f649c89a4990a63a2
shaked6540 <shaked6540@users.noreply.github.com>
2023-04-02 03:07:44 +0300

    Updated version

--- YoutubePlaylistDownloader/GlobalConsts.cs
+++ YoutubePlaylistDownloader/GlobalConsts.cs
@@ -35,7 +35,7 @@ namespace YoutubePlaylistDownloader
         public static readonly string FFmpegFilePath;
         private static readonly string ConfigFilePath;
         private static readonly string ErrorFilePath;
-        public static readonly Version VERSION = new(1, 9, 13);
+        public static readonly Version VERSION = new(1, 9, 14);
         public static bool UpdateOnExit;
         public static string UpdateSetupLocation;
         public static bool UpdateFinishedDownloading;
