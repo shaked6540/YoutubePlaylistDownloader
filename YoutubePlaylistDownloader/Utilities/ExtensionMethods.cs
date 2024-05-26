60e9a1a315cc009facd06ca9b7428d23a7421eee
toczekmj <toczekmj@gmail.com>
2024-05-26 16:54:37 +0200

    files not skipping fix

--- YoutubePlaylistDownloader/Utilities/ExtensionMethods.cs
+++ YoutubePlaylistDownloader/Utilities/ExtensionMethods.cs
@@ -14,4 +14,16 @@ public static class ExtensionMethods
 
         await Task.WhenAll(toAwait).ConfigureAwait(false);
     }
+
+    public static async Task<bool> BulkFileExists(IVideo video, int vIndex, string file, string fileType, string savePath, FullPlaylist playlist = null)
+    {
+        var fullVideo = await GlobalConsts.YoutubeClient.Videos.GetAsync(video.Id).ConfigureAwait(false);
+        string tagged = await GlobalConsts.TagMusicFile(fullVideo, file, vIndex, returnTitleOnly: true);
+        string taggedBasedOnTitle = await GlobalConsts.TagFileBasedOnTitle(video, vIndex, file, playlist, true);
+
+        var taggedPath = $"{savePath}\\{tagged}.{fileType}";
+        var titlePath = $"{savePath}\\{taggedBasedOnTitle}.{fileType}";
+
+        return File.Exists(taggedPath) || File.Exists(titlePath);
+    }
 }
